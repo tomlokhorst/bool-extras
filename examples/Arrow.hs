4f6193a06b4e460374ed7a45b4f40ef54f911e97
Tom Lokhorst <tom@lokhorst.eu>
2009-05-12 14:35:10 +0200

    Updated examples to work with new names.

--- examples/Arrow.hs
+++ examples/Arrow.hs
@@ -3,7 +3,7 @@ module Arrow where
 import Data.Bool.Extras
 
 maybePlus5 :: Bool -> Int -> Int
-maybePlus5 = boolA (+5)
+maybePlus5 b = (+5) `whenA` b
 
 main = print (maybePlus5 True 4)
 
--- examples/Monoid.hs
+++ examples/Monoid.hs
@@ -3,7 +3,7 @@ module Monoid where
 import Data.Bool.Extras
 
 xsB :: Bool -> [Int]
-xsB = boolM [1..5]
+xsB = mwhen [1..5]
 
 main = print (xsB False)
 
